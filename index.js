23e91452272f28a4d9b0e9f6087d64f4bb36ab3f
Quốc Dũng <91933007+QuocDung21@users.noreply.github.com>
2023-06-14 20:35:40 +0700

    fix

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
-
 const app = express();
 const port = 3000;
-
-// Cấu hình kết nối cơ sở dữ liệu MySQL
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "luxubu_test",
+  host: "baz4n9jsazwull2hq07k-mysql.services.clever-cloud.com",
+  user: "uptpnxygmxqdjbdx",
+  password: "L7RGh4wuYai0WmFUz84f",
+  database: "baz4n9jsazwull2hq07k",
 });
-
-// Kết nối tới cơ sở dữ liệu
 connection.connect((err) => {
   if (err) throw err;
   console.log("Connected to the database");
 });
-
-// Middleware để phân tích nội dung của yêu cầu HTTP
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.json("Api listening");
 });
-// Thêm một bài viết
 app.post("/api/blog", (req, res) => {
   const { id, title, description, idcateblog, author, images } = req.body;
-
   const sql =
     "INSERT INTO blog (id, title, description, idcateblog, author, images) VALUES (?, ?, ?, ?, ?, ?)";
   const values = [id, title, description, idcateblog, author, images];
@@ -39,8 +30,6 @@ app.post("/api/blog", (req, res) => {
     res.send("Blog inserted successfully");
   });
 });
-
-// Sửa một bài viết
 app.put("/api/blog/:id", (req, res) => {
   const { id } = req.params;
   const { title, description, idcateblog, author, images } = req.body;
@@ -55,21 +44,15 @@ app.put("/api/blog/:id", (req, res) => {
     res.send("Blog updated successfully");
   });
 });
-
-// Xóa một bài viết
 app.delete("/api/blog/:id", (req, res) => {
   const { id } = req.params;
-
   const sql = "DELETE FROM blog WHERE id = ?";
-
   connection.query(sql, id, (err, result) => {
     if (err) throw err;
     console.log("Deleted the blog with ID:", id);
     res.send("Blog deleted successfully");
   });
 });
-
-// Khởi động server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
